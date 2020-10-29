041bdbd2483dd229967296f0070fc2250461114c
Heshan <heshansandeepa94@gmail.com>
2020-10-30 02:14:46 +0530

    ReversedLinkedList function added

--- Data Structures/LinkedList.c
+++ Data Structures/LinkedList.c
@@ -13,23 +13,20 @@ void InsertAtEnd(int x);
 void Insert(int x,int n);
 int Length();
 void Delete(int n);
+void ReversedLinkedList();
 
 int main(){
     head=NULL;
     InsertAtBeginning(1);
     InsertAtBeginning(2);
     InsertAtBeginning(3);
-    InsertAtEnd(4);
     InsertAtBeginning(10);
-    InsertAtEnd(200);
-    Insert(1000,2);
-    Insert(2000,3);
     Print();
     printf("%d\n",Length());
 
     printf("------\n");
 
-    Delete(1);
+   /*Delete(1);
     Print();
     printf("%d\n",Length());
 
@@ -39,11 +36,15 @@ int main(){
 
     Delete(3);
     Print();
-    printf("%d\n",Length());
+    printf("%d\n",Length());*/
+
+    ReversedLinkedList();
+    Print();
 
     return 0;
 }
 
+//Insert a element into start of the linked list
 void InsertAtBeginning(int x){
     struct Node * temp =(struct Node*)malloc(sizeof(struct Node));
     temp->data=x;
@@ -51,6 +52,7 @@ void InsertAtBeginning(int x){
     head = temp;
 }
 
+//Print all the elements in the linked list
 void Print(){
 
     struct Node* temp=head;
@@ -62,6 +64,7 @@ void Print(){
     printf("\n");
 }
 
+//Insert a data into end of the linked list
 void InsertAtEnd(int x){
     struct Node* temp =head;
 
@@ -74,6 +77,7 @@ void InsertAtEnd(int x){
     temp->next=lastNode;
 }
 
+//Insert a data into given position/index
 void Insert(int x, int n){
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     struct Node* temp = head;
@@ -98,6 +102,7 @@ void Insert(int x, int n){
     temp->next=newNode;
 }
 
+//Return length of the linked list
 int Length(){
     struct Node* temp = head;
     int n=0;
@@ -109,6 +114,7 @@ int Length(){
     return n;
 }
 
+//Delete a node in a given position/index.
 void Delete(int n){
     struct Node* temp = head;
     struct Node* deleteNode;
@@ -132,4 +138,20 @@ void Delete(int n){
     }
 
     free(deleteNode);
+}
+
+// Reversed the linked list
+void ReversedLinkedList(){
+    struct Node* current;
+    struct Node* next;
+    struct Node* prev = NULL;
+    current = head;
+
+    while(current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current= next;
+    }
+    head = prev;
 }
